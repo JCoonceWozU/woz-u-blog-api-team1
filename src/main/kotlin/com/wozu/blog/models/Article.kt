fd7d32e78c62fed4debad5ed5818513f064cae6c
Alicia Diller <aliciad925@gmail.com>
2020-06-09 13:10:41 -0700

    created article model

---
+++ src/main/kotlin/com/wozu/blog/models/Article.kt
@@ -0,0 +1,25 @@
+package com.wozu.blog.models
+
+import java.time.OffsetDateTime
+import javax.persistence.*
+
+@Entity
+data class Article(var title: String = "",
+                   var body: String = "",
+                   var slug: String = "",
+                   //@ManyToMany
+                   //var tagList: MutableList<Tag> = mutableListOf(),
+                   var createdAt: OffsetDateTime = OffsetDateTime.now(),
+                   var updatedAt: OffsetDateTime = OffsetDateTime.now(),
+                   var favorited: Long = 0,
+                   //@ManyToOne
+                   //var author: User = User(),
+                   var isPaid: Boolean = false,
+                   var isPublished: Boolean = false,
+                   var wordCount: Long = 0,
+                   var readTime: Long = 0,
+                   @Id @GeneratedValue(strategy = GenerationType.AUTO)
+                   var id: Long = 0) {
+    //create algorithm for read time
+    fun calcReadTime() = null
+}
